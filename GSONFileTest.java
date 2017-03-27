c61bcfca9fb71f15fdf40c63214f33de597ff175
JARRAY Lamjed <lamjedjarray@gmail.com>
2017-03-27 10:50:23 +0100

    Converting a file to JsonObject with GSON
    Converting a file to JsonObject with GSON

---
+++ GSONFileTest.java
@@ -0,0 +1,34 @@
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class GSONFileTest {
+
+public static void main(String[] args)
+{
+   convertFileToJSON ("/tmp/test.json");
+}
+
+public static JsonObject convertFileToJSON (String fileName){
+
+        // Read from File to String
+        JsonObject jsonObject = new JsonObject();
+        
+        try {
+            JsonParser parser = new JsonParser();
+            JsonElement jsonElement = parser.parse(new FileReader(fileName));
+            jsonObject = jsonElement.getAsJsonObject();
+        } catch (FileNotFoundException e) {
+           
+        } catch (IOException ioe){
+        
+        }
+        
+        
+        return jsonObject;
+    }
+
+}
